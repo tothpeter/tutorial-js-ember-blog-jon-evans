23ccf2d3912ec142518e730dbcfbaa18b62fbeb9
Peter Toth <tothpeter08@gmail.com>
2015-06-01 00:41:26 +0200

    Find single post in mockup sercer

--- server/mocks/posts.js
+++ server/mocks/posts.js
@@ -1,41 +1,69 @@
+if (!Array.prototype.find) {
+  Array.prototype.find = function(predicate) {
+    if (this == null) {
+      throw new TypeError('Array.prototype.find called on null or undefined');
+    }
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+    var list = Object(this);
+    var length = list.length >>> 0;
+    var thisArg = arguments[1];
+    var value;
+
+    for (var i = 0; i < length; i++) {
+      value = list[i];
+      if (predicate.call(thisArg, value, i, list)) {
+        return value;
+      }
+    }
+    return undefined;
+  };
+}
+
+
 module.exports = function(app) {
   var express = require('express');
   var postsRouter = express.Router();
 
+  var posts = [
+    {
+      id: 1,
+      title: "Complete Ember.js Tutorial",
+      body: 'body',
+      author: 1
+    },
+    {
+      id: 2,
+      title: "Checkout some more ember stuff",
+      body: 'body2',
+      author: 2
+    },
+    {
+      id: 3,
+      title: "Solve world hunger (with Ember)",
+      body: 'body3',
+      author: 1
+    }
+  ];
+
+  var authors = [
+    {
+      id: 1,
+      name: 'Toma',
+      // posts: [1,3]
+    },
+    {
+      id: 2,
+      name: 'Author2',
+      // posts: [2]
+    }
+  ];
+
   postsRouter.get('/', function(req, res) {
     res.send({
-      'posts': [
-        {
-          id: 1,
-          title: "Complete Ember.js Tutorial",
-          body: 'body',
-          author: 1
-        },
-        {
-          id: 2,
-          title: "Checkout some more ember stuff",
-          body: 'body2',
-          author: 2
-        },
-        {
-          id: 3,
-          title: "Solve world hunger (with Ember)",
-          body: 'body3',
-          author: 1
-        }
-      ],
-      'authors': [
-        {
-          id: 1,
-          name: 'Toma',
-          // posts: [1,3]
-        },
-        {
-          id: 2,
-          name: 'Author2',
-          // posts: [2]
-        }
-      ]
+      'posts': posts,
+      'authors': authors
     });
   });
 
@@ -45,9 +73,10 @@ module.exports = function(app) {
 
   postsRouter.get('/:id', function(req, res) {
     res.send({
-      'posts': {
-        id: req.params.id
-      }
+      'post': posts.find(function(post) {
+        return post.id == req.params.id;
+      }),
+      'authors': authors
     });
   });
 
