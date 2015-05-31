4224b456b2b7a9f9be06a44e6510b32cdf5d34b0
Peter Toth <tothpeter08@gmail.com>
2015-05-31 21:21:27 +0200

    Use restful adapter and add json api server for posts

--- app/models/post.js
+++ app/models/post.js
@@ -2,19 +2,4 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr('string')
-}).reopenClass({
-  FIXTURES: [
-    {
-      id: 1,
-      title: "Complete Ember.js Tutorial",
-    },
-    {
-      id: 2,
-      title: "Checkout some more ember stuff",
-    },
-    {
-      id: 3,
-      title: "Solve world hunger (with Ember)",
-    }
-  ]
 });
---
+++ server/mocks/posts.js
@@ -0,0 +1,49 @@
+module.exports = function(app) {
+  var express = require('express');
+  var postsRouter = express.Router();
+
+  postsRouter.get('/', function(req, res) {
+    res.send({
+      'posts': [
+        {
+          id: 1,
+          title: "Complete Ember.js Tutorial",
+        },
+        {
+          id: 2,
+          title: "Checkout some more ember stuff",
+        },
+        {
+          id: 3,
+          title: "Solve world hunger (with Ember)",
+        }
+      ]
+    });
+  });
+
+  postsRouter.post('/', function(req, res) {
+    res.status(201).end();
+  });
+
+  postsRouter.get('/:id', function(req, res) {
+    res.send({
+      'posts': {
+        id: req.params.id
+      }
+    });
+  });
+
+  postsRouter.put('/:id', function(req, res) {
+    res.send({
+      'posts': {
+        id: req.params.id
+      }
+    });
+  });
+
+  postsRouter.delete('/:id', function(req, res) {
+    res.status(204).end();
+  });
+
+  app.use('/api/posts', postsRouter);
+};
